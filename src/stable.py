b763a26dddfa42ec175d66a66b4a258fe352906c
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-21 19:11:54 +0100

    Update stable.py

--- src/stable.py
+++ src/stable.py
@@ -4,18 +4,19 @@ import time
 import json
 import itertools
 from src.utils.response_processor import ResponseProcessor
+from src.utils.image_utils import fetch_images
+from src.utils.image_upload import ImageUploader
 
 class StableAPI:
-
     BASE_URL = 'https://stablediffusionapi.com/api'
     CONFIG_PATH = './config/stable'
     HEADERS = {"Content-Type": "application/json"}
 
     def __init__(self, api_key=None):
         self.api_key = api_key
+        self.uploader = ImageUploader()
 
-    @staticmethod
-    def _load_yaml(file):
+    def _load_yaml(self, file):
         with open(file, 'r') as f:
             return yaml.safe_load(f)
 
@@ -28,8 +29,8 @@ class StableAPI:
             return None
 
     def request(self, call=None, **kwargs):
-        url = f'{self.BASE_URL}/v5/controlnet' if call == 'controlnet' else f'{self.BASE_URL}/v3/{call}'
-        yaml_file = f'{self.CONFIG_PATH}/controlnet.yml' if call == 'controlnet' else f'{self.CONFIG_PATH}/{call}.yml'
+        url = f'{self.BASE_URL}/v3/{call}'
+        yaml_file = f'{self.CONFIG_PATH}/{call}.yml'
 
         api_options = self._load_yaml(yaml_file)
         api_options.update(kwargs)
@@ -44,12 +45,9 @@ class StableAPI:
         responses = []
         for combo in combos:
             response_data = self.request(**combo)
-            if debug:
-                self.debug_message(combo, response_data)
+            self.debug_message(combo, response_data) if debug else None
             responses.append(response_data)
-            status = response_data['status']
-            
-        return responses, status
+        return responses, response_data['status']
 
     @staticmethod
     def debug_message(combo, response_data):
@@ -59,3 +57,39 @@ class StableAPI:
             print(str(response_data['output']) + '\n')
         elif status == 'processing':
             print('Processing Image. Run fetch after ' + str(round(float(response_data['eta']), 2)) + ' sec.\n')
+
+    def upload_image(self, image_path):
+        return self.uploader.upload_img(image_path)
+
+    def set_options(self, stable_call, init_path=None, mask_path=None):
+        stable_calls = [stable_call]
+        options = {'call': stable_calls}
+
+        if stable_call in ["img2img", "inpaint"]:
+            print(f'uploading init_image: {init_path}')
+            init_image = self.upload_image(init_path)
+            options['init_image'] = [init_image]
+
+        if stable_call == "inpaint":
+            print(f'uploading mask_image: {mask_path}')
+            mask_image = self.upload_image(mask_path)  
+            options['mask_image'] = [mask_image]
+
+        return options
+
+    def process_responses(self, results):
+        for response_data in results:
+            processor = ResponseProcessor(response_data)
+            status, response, path = processor.process()
+            time.sleep(1)
+        return status
+
+    def fetch_images_if_processing(self, status):
+        if status == 'processing':
+            file_path='/content/unstable/output/images/processing.json'
+            fetch_images(file_path, self.api_key)
+
+    def fetch_images_from_path(self, file_path, stable_debug):
+        if stable_debug:
+            print('Fetching Images from' + file_path)
+        fetch_images(file_path, self.api_key)
