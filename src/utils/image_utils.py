b4e7d7787a7911839f9d85f1d3d0185f78d71f2c
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-21 12:05:32 +0100

    Update image_utils.py

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -2,14 +2,20 @@ import os
 import requests
 import shutil
 
-
 def download_img(url, path):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     response = requests.get(url, stream=True)
 
     if response.status_code == 200:
-        with open(path, 'wb') as f:
+        filename = os.path.basename(url) if not os.path.basename(path) else os.path.basename(path)
+        full_path = os.path.join(os.path.dirname(path), filename)
+
+        with open(full_path, 'wb') as f:
             response.raw.decode_content = True
             shutil.copyfileobj(response.raw, f)
+
+        return full_path
     else:
         print(f"Error downloading image: {response.status_code} - {response.text}")
+        return None
+
