7cb126282780d69c1d3e8b08079168b7ce232a37
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-24 21:23:58 +0100

    Update stable.py

--- src/stable.py
+++ src/stable.py
@@ -14,9 +14,12 @@ class StableAPI:
     CONFIG_PATH = './config/stable'
     HEADERS = {"Content-Type": "application/json"}
 
-    def __init__(self, api_key=None):
+    def __init__(self, api_key=None, yaml_path=None, debug=False, fetch_only=False):
         self.api_key = api_key
         self.uploader = ImageUploader()
+        self.yaml_path = yaml_path
+        self.debug = debug
+        self.fetch_only = fetch_only
 
     @staticmethod
     def _load_yaml(file):
@@ -38,42 +41,9 @@ class StableAPI:
         api_options['key'] = self.api_key
         return self._make_request(url, api_options)
 
-    def get_responses(self, options_dict, debug=False):
-        combos = [dict(zip(options_dict, v)) for v in itertools.product(*options_dict.values())]
-        responses = [self.request(**combo) for combo in combos]
-        if debug:
-            for combo, response_data in zip(combos, responses):
-                self.debug_message(combo, response_data)
-        return responses, responses[0]['status']
-
-    @staticmethod
-    def debug_message(combo, response_data):
-        print(f'Rendering: {combo}')
-        status = response_data['status']
-        if status == 'success':
-            print(f"{response_data['output']}\n")
-        elif status == 'processing':
-            print(f'Processing Image. Run fetch after {round(float(response_data["eta"]), 2)} sec.\n')
-
-    def upload_and_set_image(self, options, image_key):
-        paths = options.get(image_key)
-
-        if not paths:
-            return
-
-        if not isinstance(paths, list):
-            paths = [paths]
-
-        uploaded_images = []
-        for path in paths:
-            if os.path.isfile(path):
-                print(f'uploading {image_key}: {path}')
-                uploaded_images.append(self.uploader.upload_img(path))
-            else:
-                raise FileNotFoundError(f"No file found at {path}")
-        options[image_key] = uploaded_images
-
-    def set_options(self, yaml_path):
+    def set_options(self, yaml_path=None):
+        if yaml_path is None:
+            yaml_path = self.yaml_path
         options = self.yml_to_options(yaml_path)
 
         if options.get('init_image'):
@@ -83,6 +53,48 @@ class StableAPI:
             self.upload_and_set_image(options, 'mask_image')
 
         return options
+
+    def run(self):
+        if self.fetch_only:
+            self.fetch_images_from_path(self.yaml_path, self.debug)
+            return
+
+        options = self.set_options()
+        responses, status = self.get_responses(options)
+        self.process_responses(responses)
+        if status == 'processing':
+            self.fetch_images_if_processing()
+
+    def get_responses(self, options_dict):
+        combos = [dict(zip(options_dict, v)) for v in itertools.product(*options_dict.values())]
+        responses = [self.request(**combo) for combo in combos]
+        if self.debug:
+            self.debug_responses(combos, responses)
+        return responses, responses[0]['status']
+
+    @staticmethod
+    def debug_responses(combos, responses):
+        for combo, response_data in zip(combos, responses):
+            print(f'Rendering: {combo}')
+            status = response_data['status']
+            if status == 'success':
+                print(f"{response_data['output']}\n")
+            elif status == 'processing':
+                print(f'Processing Image. Run fetch after {round(float(response_data["eta"]), 2)} sec.\n')
+
+    def upload_and_set_image(self, options, image_key):
+        paths = options.get(image_key)
+        if not paths: return
+        if not isinstance(paths, list): paths = [paths]
+
+        uploaded_images = []
+        for path in paths:
+            if os.path.isfile(path):
+                # print(f'uploading {image_key}: {path}')
+                uploaded_images.append(self.uploader.upload_img(path))
+            else:
+                raise FileNotFoundError(f"No file found at {path}")
+        options[image_key] = uploaded_images
 
     @staticmethod
     def yml_to_options(filename):
@@ -102,11 +114,16 @@ class StableAPI:
             ResponseProcessor(response_data).process()
             time.sleep(1)
 
-    def fetch_images_if_processing(self, status):
-        if status == 'processing':
-            fetch_images('/content/unstable/output/images/processing.json', self.api_key)
+    def fetch_images_if_processing(self):
+        fetch_images('/content/unstable/output/images/processing.json', self.api_key)
 
-    def fetch_images_from_path(self, file_path, stable_debug):
+    def fetch_images_from_path(self, file_path=None, stable_debug=None):
+        if stable_debug is None:
+            stable_debug = self.debug
+        if file_path is None:
+            file_path = self.yaml_path
+
         if stable_debug:
             print('Fetching Images from' + file_path)
         fetch_images(file_path, self.api_key)
+
