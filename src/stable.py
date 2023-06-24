73a04b80a83dbd9bc3b0f5c1b6a28e4205d09b0a
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-24 17:55:48 +0100

    Update stable.py

--- src/stable.py
+++ src/stable.py
@@ -1,11 +1,12 @@
+import os
 import requests
 import yaml
 import time
 import json
 import itertools
+from src.utils.image_upload import ImageUploader
 from src.utils.response_processor import ResponseProcessor
 from src.utils.image_utils import fetch_images
-from src.utils.image_upload import ImageUploader
 from src.utils.sys_utils import str2list
 
 class StableAPI:
@@ -17,28 +18,8 @@ class StableAPI:
         self.api_key = api_key
         self.uploader = ImageUploader()
 
-    def yml_to_options(self, filename):
-        # Load the options from the yaml file
-        with open(filename, 'r') as stream:
-            config = yaml.safe_load(stream)
-
-        # Create the options dictionary with loaded inputs
-        options_batch = {}
-
-        for key, value in config.items():
-            if key == "prompt":
-                # Format 'prompt' values accordingly
-                options_batch[key] = [p for p in value]
-            elif key == "negative_prompt":
-                # 'negative_prompt' values can be assigned directly
-                options_batch[key] = value
-            else:
-                # Other key-value pairs are processed here
-                options_batch[key] = str2list(value) if isinstance(value, str) else value
-
-        return options_batch
-
-    def _load_yaml(self, file):
+    @staticmethod
+    def _load_yaml(file):
         with open(file, 'r') as f:
             return yaml.safe_load(f)
 
@@ -52,64 +33,78 @@ class StableAPI:
 
     def request(self, call=None, **kwargs):
         url = f'{self.BASE_URL}/v3/{call}'
-        yaml_file = f'{self.CONFIG_PATH}/{call}.yml'
-
-        api_options = self._load_yaml(yaml_file)
+        api_options = self._load_yaml(f'{self.CONFIG_PATH}/{call}.yml')
         api_options.update(kwargs)
         api_options['key'] = self.api_key
-
         return self._make_request(url, api_options)
 
     def get_responses(self, options_dict, debug=False):
-        keys, values = zip(*options_dict.items())
-        combos = [dict(zip(keys, v)) for v in itertools.product(*values)]
-
-        responses = []
-        for combo in combos:
-            response_data = self.request(**combo)
-            self.debug_message(combo, response_data) if debug else None
-            responses.append(response_data)
-        return responses, response_data['status']
+        combos = [dict(zip(options_dict, v)) for v in itertools.product(*options_dict.values())]
+        responses = [self.request(**combo) for combo in combos]
+        if debug:
+            for combo, response_data in zip(combos, responses):
+                self.debug_message(combo, response_data)
+        return responses, responses[0]['status']
 
     @staticmethod
     def debug_message(combo, response_data):
-        print('Rendering: ' + str(combo))
+        print(f'Rendering: {combo}')
         status = response_data['status']
         if status == 'success':
-            print(str(response_data['output']) + '\n')
+            print(f"{response_data['output']}\n")
         elif status == 'processing':
-            print('Processing Image. Run fetch after ' + str(round(float(response_data['eta']), 2)) + ' sec.\n')
+            print(f'Processing Image. Run fetch after {round(float(response_data["eta"]), 2)} sec.\n')
 
-    def upload_image(self, image_path):
-        return self.uploader.upload_img(image_path)
+    def upload_and_set_image(self, options, image_key):
+        paths = options.get(image_key)
 
-    def set_options(self, stable_call, init_path=None, mask_path=None):
-        stable_calls = [stable_call]
-        options = {'call': stable_calls}
+        if not paths:
+            return
 
-        if stable_call in ["img2img", "inpaint"]:
-            print(f'uploading init_image: {init_path}')
-            init_image = self.upload_image(init_path)
-            options['init_image'] = [init_image]
+        if not isinstance(paths, list):
+            paths = [paths]
 
-        if stable_call == "inpaint":
-            print(f'uploading mask_image: {mask_path}')
-            mask_image = self.upload_image(mask_path)  
-            options['mask_image'] = [mask_image]
+        uploaded_images = []
+        for path in paths:
+            if os.path.isfile(path):
+                print(f'uploading {image_key}: {path}')
+                uploaded_images.append(self.uploader.upload_img(path))
+            else:
+                raise FileNotFoundError(f"No file found at {path}")
+        options[image_key] = uploaded_images
+
+    def set_options(self, yaml_path):
+        options = self.yml_to_options(yaml_path)
+
+        if options.get('init_image'):
+            self.upload_and_set_image(options, 'init_image')
+
+        if options.get('call') == "inpaint" and options.get('mask_image'):
+            self.upload_and_set_image(options, 'mask_image')
 
         return options
 
+    @staticmethod
+    def yml_to_options(filename):
+        with open(filename, 'r') as stream:
+            config = yaml.safe_load(stream)
+
+        options_batch = {}
+        for key, value in config.items():
+            if key in ["prompt", "init_image"]:
+                options_batch[key] = [p for p in value]
+            else:
+                options_batch[key] = str2list(value) if isinstance(value, str) else value
+        return options_batch
+
     def process_responses(self, results):
         for response_data in results:
-            processor = ResponseProcessor(response_data)
-            status, response, path = processor.process()
+            ResponseProcessor(response_data).process()
             time.sleep(1)
-        return status
 
     def fetch_images_if_processing(self, status):
         if status == 'processing':
-            file_path='/content/unstable/output/images/processing.json'
-            fetch_images(file_path, self.api_key)
+            fetch_images('/content/unstable/output/images/processing.json', self.api_key)
 
     def fetch_images_from_path(self, file_path, stable_debug):
         if stable_debug:
