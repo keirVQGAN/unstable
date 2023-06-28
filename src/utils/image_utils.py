dcab08299553fafdccf5da9c071f246d16e33012
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-28 16:11:32 +0100

    Update image_utils.py

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -2,38 +2,62 @@ import os
 import requests
 import shutil
 import json
+import math
+import textwrap
 import matplotlib.pyplot as plt
 from PIL import Image
 from IPython.display import display
+from io import BytesIO
 
 
-def img_to_grid(path, ext="png"):
-    images = []
-    # recursive search for png images
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(ext):
-                images.append(os.path.join(root, file))
+def img_to_grid(path, save_path, dpi=72, unique_meta=False, width=3):
+    with open(path, 'r') as f:
+        data = json.load(f)
 
-    # determine the grid size
-    num_images = len(images)
-    grid_size = int(num_images ** 0.5)
+    # Determine the grid size based on the length of the data
+    total_images = len(data)
+    # Adjust width if total number of images is less than the default width
+    width = min(width, total_images)
+    height = math.ceil(total_images / width)
 
-    # create subplot grid
-    fig, axs = plt.subplots(grid_size, grid_size, figsize=(20, 20))
+    # Calculate figure size (in inches) based on desired DPI and pixel dimensions
+    fig_width_in = 3000 / dpi  # Max width in inches
+    fig_height_in = (fig_width_in / width) * height  # Proportional height
 
-    # fill grid with images
-    for i, ax in enumerate(axs.flat):
-        if i < num_images:
-            img = Image.open(images[i])
+    # Calculate font size and wrap length based on width
+    font_size = 10 * (3 / width)  # Increase font size when there are fewer columns
+    wrap_length = int(0.2 * (3000 / width))  # Approx. 20% of the image's pixel width
+
+    fig, axs = plt.subplots(height, width, figsize=(fig_width_in, fig_height_in))
+    keys = ["guidance_scale", "strength", "seed", "steps", "H", "W"]
+    seen_meta = {}
+
+    for i, ax in enumerate(axs.flatten()):
+        if i < total_images:
+            entry = data[i]
+            image_url = entry['output'][0]
+            response = requests.get(image_url)
+            img = Image.open(BytesIO(response.content))
             ax.imshow(img)
-            ax.axis('off')  # don't show the axes for each image
-        else:
-            ax.remove()  # remove the extra axes without images
+            ax.axis('off')
 
-    # show the grid of images
-    plt.show()
+            meta_info = {k: entry['meta'][k] for k in keys if k in entry['meta']}
 
+            if unique_meta:
+                meta_info = {k: v for k, v in meta_info.items() if k not in seen_meta or seen_meta[k] != v}
+                seen_meta.update(meta_info)
+
+            meta_info_str = ', '.join([f"{k}: {v}" for k, v in meta_info.items()])
+            wrapped_meta_info = textwrap.fill(meta_info_str, wrap_length)
+            ax.set_title(wrapped_meta_info, fontsize=font_size)
+
+    # Turn off axes for extra subplots if total_images is not a perfect square
+    if total_images < width * height:
+        for ax in axs.flatten()[total_images:]:
+            ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(save_path, dpi=dpi)
 
 
 def image_download(url, path):
