66b1311f79cc416fced898a8d83bd9322cb7f20d
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-06-29 17:14:04 +0100

    Update transcript_utils.py

--- src/utils/transcript_utils.py
+++ src/utils/transcript_utils.py
@@ -1,11 +1,15 @@
 import yaml
 import json
 import os
+import shutil
 from src.chatbot import Chatbot
 from src.transcript_cleaner import TranscriptCleaner
 
+def read_file(file_path):
+    with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+        return file.read()
+
 def clean_transcript(transcript_path, excel_path, student_name, tutor_name, debug=True):
-    # Instantiate the TranscriptCleaner class
     transcript_cleaner = TranscriptCleaner(
         transcript_path=transcript_path,
         excel_path=excel_path,
@@ -14,25 +18,26 @@ def clean_transcript(transcript_path, excel_path, student_name, tutor_name, debu
         debug=debug
     )
 
-    # Call the clean_transcript method
     transcript_clean = transcript_cleaner.clean_transcript()
-
-    # Call the clean_pre_tutorial_notes method
     pre_notes_clean = transcript_cleaner.clean_pre_tutorial_notes()
 
     return transcript_clean, pre_notes_clean
 
 
 def transcript(transcript_path, pre_notes_path, openai_api_key, transcript_yml='/content/unstable/config/transcript/config_transcript.yml', model='gpt-4', count_tokens=False, chat=False, debug=False, tutor_name='Keir Williams'):
-    # Extracting student's name from the transcript file name
     student_name = os.path.basename(transcript_path).replace('.txt', '')
+    student_folder = os.path.join(os.getcwd(), student_name)
+
+    os.makedirs(student_folder, exist_ok=True)
+
+    transcript_unclean = read_file(transcript_path)
+    pre_notes_unclean = read_file(pre_notes_path)
 
     transcript_clean, pre_notes_clean = clean_transcript(
         transcript_path=transcript_path,
         excel_path=pre_notes_path,
         student_name=student_name,
         tutor_name=tutor_name,
-        # json_file_path=json_file_path,
         debug=debug
     )
 
@@ -56,14 +61,12 @@ def transcript(transcript_path, pre_notes_path, openai_api_key, transcript_yml='
         chatbot_instance.chat(count_tokens=count_tokens)
     else:
         response, token_usage = chatbot_instance.chat(user_message, count_tokens=True) if count_tokens else (chatbot_instance.chat(user_message), None)
-        print(f'Saved to {student_name}_summary.txt')
-        print()
+        print(f'Saved to {os.path.join(student_folder, student_name)}_summary.txt')
         print(response, token_usage)
 
         # Save response as text file with student name
-        with open(f'{student_name}_summary.txt', 'w') as f:
+        with open(os.path.join(student_folder, f'{student_name}_summary.txt'), 'w') as f:
             f.write(response)
-
 
         # Save cleaned text and summary as json file with student name
         data = {
@@ -71,5 +74,16 @@ def transcript(transcript_path, pre_notes_path, openai_api_key, transcript_yml='
             "cleaned_pre_notes": pre_notes_clean,
             "summary": response
         }
-        with open(f'{student_name}.json', 'w') as f:
+        with open(os.path.join(student_folder, f'{student_name}.json'), 'w') as f:
             json.dump(data, f)
+
+        # Save uncleaned transcript and pre-notes as json file with student name
+        data_unclean = {
+            "uncleaned_transcript": transcript_unclean,
+            "uncleaned_pre_notes": pre_notes_unclean
+        }
+        with open(os.path.join(student_folder, f'{student_name}_unclean_data.json'), 'w') as f:
+            json.dump(data_unclean, f)
+
+        # Copy the original transcript file to the student's folder
+        shutil.copy(transcript_path, os.path.join(student_folder, os.path.basename(transcript_path) + '_transcript'))
