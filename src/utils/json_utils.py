46a22d9da0ff6709afa46cd29061ae8823da37ca
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-07-09 10:10:00 +0100

    Update json_utils.py

--- src/utils/json_utils.py
+++ src/utils/json_utils.py
@@ -1,5 +1,21 @@
 import json
 from pathlib import Path
+import os
+
+def image_to_yml(directory):
+    yml_str = "init_image:\n"
+    
+    # Use os.walk to iterate over all files in directory and its subdirectories
+    for root, _, files in os.walk(directory):
+        for file in files:
+            # Check if the file is an image (assuming only .jpg, .jpeg, .png files are images)
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                # Concatenate the root directory and file name to get the full file path
+                full_file_path = os.path.join(root, file)
+                # Add the file path to the YAML string
+                yml_str += f"  - \"{full_file_path}\"\n"
+    print(yml_str)
+    return yml_str
 
 
 def write_to_json(data, path):
