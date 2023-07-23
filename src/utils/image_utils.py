e51a065356022d3ed4e792a94397f7a5f56a55bb
keirVQGAN <99478252+keirVQGAN@users.noreply.github.com>
2023-07-23 16:15:07 +0100

    Update image_utils.py

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -1,116 +1,108 @@
+# image_utils.py
+
 import os
 import requests
 import shutil
 import json
-import math
-import textwrap
-import matplotlib.pyplot as plt
-from PIL import Image
-from IPython.display import display
-from io import BytesIO
-from src.utils.json_utils import write_to_json, append_to_json
 from pathlib import Path
-from datetime import datetime
+from PIL import Image
+from io import BytesIO
+
+import matplotlib.pyplot as plt
+
+from src.utils import (
+    json_utils,
+    sys_utils
+)
+
 
 def fetch_images(file_path, json_output_path, api_key):
     data = json.loads(Path(file_path).read_text())
 
     updated_data = []
-    fetched_data = []  # To store data of successfully fetched images
+    fetched_data = []  
 
     for item in data:
-        response_data = send_request(item['id'], api_key)
-        output_path = f'./output/images/{item["id"]}'
-
-        if response_data.get('status') == 'success' and response_data.get('output'):
+        response = send_request(item['id'], api_key)
+        
+        if response['status'] == 'success' and response['output']:
             try:
-                download_images(response_data['output'], output_path)
-                response_data['meta'] = get_meta_data(item['id'], Path(file_path).parent)
-                response_data['date_time'] = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                fetched_data.append(response_data)
+                download_images(response['output'], f'./output/images/{item["id"]}')
+                response['meta'] = get_meta_data(item['id'], Path(file_path).parent)
+                fetched_data.append(response)
             except Exception as e:
-                print(f"An error occurred while downloading images: {e}")
+                print(f"Error downloading images: {e}")
             finally:
                 updated_data.append(item)
 
-        elif response_data.get('status') == 'processing':
-            print(f"Image isn't ready to fetch yet. Try again at {item.get('available')}")
+        elif response['status'] == 'processing':
+            print(f"Image not ready. Try again at {item.get('available')}")
             updated_data.append(item)
         else:
             updated_data.append(item)
 
-    Path(file_path).write_text(json.dumps(updated_data, indent=4, separators=(',', ': ')))
+    Path(file_path).write_text(json.dumps(updated_data, indent=4))
 
-    # Write fetched data to fetched.json
-    fetched_file_path = json_output_path
     for fetched_item in fetched_data:
-        append_to_json(fetched_item, fetched_file_path)
+        json_utils.append_to_json(fetched_item, json_output_path)
 
 
 def send_request(id, api_key):
-    url = f"https://stablediffusionapi.com/api/v3/fetch/{id}"
+    url = f"https://stablediffusionapi.com/api/v3/fetch/{id}" 
     headers = {'Content-Type': 'application/json'}
-    post_data = json.dumps({"key": api_key})
-    return requests.post(url, headers=headers, data=post_data).json()
+    data = {"key": api_key}
+    return requests.post(url, headers=headers, json=data).json()
 
 
 def download_images(image_urls, output_path):
-    for image_url in image_urls:
-        image_download(image_url, f'{output_path}/{os.path.basename(image_url)}')
-        print(f"Successfully downloaded image from {image_url}")
+    for url in image_urls:
+        filename = os.path.basename(url)
+        image_download(url, f'{output_path}/{filename}')
+        print(f"Downloaded {url}")
 
 
 def get_meta_data(id, base_path):
-    # Path to the specific JSON file
-    json_file_path = base_path / f"{id}/json/{id}.json"
-    data = json.loads(json_file_path.read_text())
+    json_path = base_path / f"{id}/json/{id}.json"
+    data = json.loads(json_path.read_text())
     return data.get('meta', {})
 
 
 def img_to_grid(path, save_path, dpi=72, unique_meta=False, width=3):
-    with open(path, 'r') as f:
+    with open(path) as f:
         data = json.load(f)
 
-    # Determine the grid size based on the length of the data
-    total_images = len(data)
-    # Adjust width if total number of images is less than the default width
-    width = min(width, total_images)
-    height = math.ceil(total_images / width)
+    total = len(data)
+    width = min(width, total) 
+    height = math.ceil(total / width)
 
-    # Calculate figure size (in inches) based on desired DPI and pixel dimensions
-    fig_width_in = 3000 / dpi  # Max width in inches
-    fig_height_in = (fig_width_in / width) * height  # Proportional height
+    fig_width = 3000 / dpi  
+    fig_height = (fig_width / width) * height
 
-    # Calculate font size and wrap length based on width
-    font_size = 10 * (3 / width)  # Increase font size when there are fewer columns
-    wrap_length = int(0.2 * (3000 / width))  # Approx. 20% of the image's pixel width
+    font_size = 10 * (3 / width)
+    wrap_length = int(0.2 * (3000 / width))
 
-    fig, axs = plt.subplots(height, width, figsize=(fig_width_in, fig_height_in))
+    fig, axs = plt.subplots(height, width, figsize=(fig_width, fig_height))
     keys = ["guidance_scale", "strength", "seed", "steps", "H", "W"]
     seen_meta = {}
 
     for i, ax in enumerate(axs.flatten()):
-        if i < total_images:
+        if i < total:
             entry = data[i]
-            image_url = entry['output'][0]
-            response = requests.get(image_url)
-            img = Image.open(BytesIO(response.content))
+            img = Image.open(BytesIO(requests.get(entry['output'][0]).content))
             ax.imshow(img)
             ax.axis('off')
 
-            meta_info = {k: entry['meta'][k] for k in keys if k in entry['meta']}
-
+            meta = {k: entry['meta'][k] for k in keys if k in entry['meta']}
             if unique_meta:
-                meta_info = {k: v for k, v in meta_info.items() if k not in seen_meta or seen_meta[k] != v}
-                seen_meta.update(meta_info)
+                meta = {k: v for k, v in meta.items() if k not in seen_meta or seen_meta[k] != v}
+                seen_meta.update(meta)
 
-            meta_info_str = ', '.join([f"{k}: {v}" for k, v in meta_info.items()])
-            wrapped_meta_info = textwrap.fill(meta_info_str, wrap_length)
-            ax.set_title(wrapped_meta_info, fontsize=font_size)
+            meta_str = ', '.join([f"{k}: {v}" for k, v in meta.items()])
+            wrapped_meta = textwrap.fill(meta_str, wrap_length)
+            ax.set_title(wrapped_meta, fontsize=font_size)
 
-    # Turn off axes for extra subplots if total_images is not a perfect square
-    if total_images < width * height:
-        for ax in axs.flatten()[total_images:]:
+    if total < width * height:
+        for ax in axs.flatten()[total:]:
             ax.axis('off')
 
     plt.tight_layout()
@@ -122,145 +114,155 @@ def image_download(url, path):
     response = requests.get(url, stream=True)
 
     if response.status_code == 200:
-        filename = os.path.basename(url) if not os.path.basename(path) else os.path.basename(path)
-        full_path = os.path.join(os.path.dirname(path), filename)
-
-        with open(full_path, 'wb') as f:
+        with open(path, 'wb') as f:
             response.raw.decode_content = True
             shutil.copyfileobj(response.raw, f)
-
-        return full_path
-    else:
-        print(f"Error downloading image: {response.status_code} - {response.text}")
-        return None
+        return path
+    
+    print(f"Error downloading {url}: {response.status_code} - {response.text}")
 
 
 def web_grid(path, bg_color, dir_name="web_files"):
-    with open(path, 'r') as f:
+    with open(path) as f:
         data = json.load(f)
 
-    if not os.path.exists(dir_name):
-        os.makedirs(dir_name)
+    sys_utils.create_dirs(dir_name) 
 
-    with open(os.path.join(dir_name, 'index.html'), 'w') as file:
-        file.write(generate_html(data, bg_color))
+    with open(f"{dir_name}/index.html", 'w') as f:
+        f.write(generate_html(data, bg_color))
 
-    with open(os.path.join(dir_name, 'style.css'), 'w') as file:
-        file.write(generate_css(bg_color))
+    with open(f"{dir_name}/style.css", 'w') as f:
+        f.write(generate_css(bg_color))
 
-    with open(os.path.join(dir_name, 'main.js'), 'w') as file:
-        file.write(generate_js(data))
+    with open(f"{dir_name}/main.js", 'w') as f:
+        f.write(generate_js(data))
+
 
 def generate_html(data, bg_color):
     common_meta = {k: data[0]['meta'][k] for k in ["guidance_scale", "strength", "seed", "steps", "H", "W"] if k in data[0]['meta']}
-    common_meta['prompt'] = data[0]['meta'].get('prompt', '')  # Add 'prompt' key to common_meta
+    common_meta['prompt'] = data[0]['meta'].get('prompt', '')  
 
     unique_meta_list = []
 
     for entry in data:
-        meta_info = {k: entry['meta'][k] for k in ["guidance_scale", "strength", "seed", "steps", "H", "W", "prompt"] if k in entry['meta']}
-        unique_meta = {k: v for k, v in meta_info.items() if k not in common_meta or common_meta[k] != v}
+        meta = {k: entry['meta'][k] for k in ["guidance_scale", "strength", "seed", "steps", "H", "W", "prompt"] if k in entry['meta']}
+        unique_meta = {k: v for k, v in meta.items() if k not in common_meta or common_meta[k] != v}
         unique_meta_list.append(unique_meta)
 
-    # Start building the HTML
-    html = f"""<!DOCTYPE html>
-<html>
-<head>
-    <title>Image Grid</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body style="background-color: {bg_color};">
-    <div class="grid">"""
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Image Grid</title>
+      <link rel="stylesheet" href="style.css"> 
+    </head>
+    <body style="background-color: {bg_color};">
+      <div class="grid">
+    """
 
     for i, entry in enumerate(data):
-        image_url = entry['output'][0]
-        meta_info_str = ', '.join([f"{k}: {v}" for k, v in unique_meta_list[i].items()])
+        img_url = entry['output'][0]
+        meta_str = ', '.join([f"{k}: {v}" for k, v in unique_meta_list[i].items()])
         html += f"""
-        <div class="cell" data-url="{image_url}" data-meta="{meta_info_str}" onclick="downloadImage(this)">
-            <img src="{image_url}" alt="image">
-            <div class="overlay">
-                <div class="text">{meta_info_str}</div>
-            </div>
-        </div>"""
+        <div class="cell" data-url="{img_url}" data-meta="{meta_str}" onclick="downloadImage(this)">
+          <img src="{img_url}">
+          <div class="overlay">
+            <div class="text">{meta_str}</div>
+          </div>
+        </div>
+        """
 
     common_meta_str = ', '.join([f"{k}: {v}" for k, v in common_meta.items()])
 
     html += f"""
-    </div>
-    <div class="common-info">
-        <p><strong>Common Meta Information:</strong> {common_meta_str}</p>
-    </div>
-    <script src="main.js"></script>
-</body>
-</html>"""
+      </div>
+
+      <div class="common-info">
+        <p><strong>Common Meta:</strong> {common_meta_str}</p> 
+      </div>
+
+      <script src="main.js"></script>
+
+    </body>
+    </html>
+    """
+
     return html
 
+
 def generate_css(bg_color):
-    return f"""
+  return f"""
     body {{
-        font-family: Arial, sans-serif;
-        margin: 0;
-        padding: 0;
-        background-color: {bg_color};
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+      background-color: {bg_color};
     }}
+    
     .grid {{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: center;
-        padding: 50px;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+      padding: 50px;
     }}
+    
     .cell {{
-        margin: 10px;
-        position: relative;
+      margin: 10px;
+      position: relative;
     }}
+    
     .cell img {{
-        width: 200px;
-        height: 200px;
+      width: 200px;
+      height: 200px;
     }}
+    
     .overlay {{
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        height: 200px;
-        width: 200px;
-        opacity: 0;
-        transition: .3s ease;
-        background-color: black;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      height: 200px;
+      width: 200px;
+      opacity: 0;
+      transition: .3s ease;
+      background-color: black; 
     }}
+    
     .cell:hover .overlay {{
-        opacity: 0.8;
+      opacity: 0.8;
     }}
+    
     .text {{
-        color: white;
-        font-size: 12px;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        text-align: center;
+      color: white;
+      font-size: 12px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      text-align: center;
     }}
+    
     .common-info {{
-        max-width: 800px;
-        margin: 50px auto;
-        padding: 20px;
-        border: 1px solid #333;
-        border-radius: 5px;
+      max-width: 800px;
+      margin: 50px auto;  
+      padding: 20px;
+      border: 1px solid #333;
+      border-radius: 5px;
     }}
-    """
+  """
 
 def generate_js(data):
-    return """
+  return """
     function downloadImage(element) {
-        var url = element.getAttribute('data-url');
-        var meta = element.getAttribute('data-meta');
-        var link = document.createElement('a');
-        link.href = url;
-        link.download = meta.replace(/, /g, '_') + '.png';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+      let url = element.getAttribute('data-url');
+      let meta = element.getAttribute('data-meta');
+      let link = document.createElement('a');
+      link.href = url;
+      link.download = meta.replace(/, /g, '_') + '.png';
+      
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
     }
-    """
-
+  """
